6d04efcd068e78757ca800f7a3c437032e75647a
Vanda89 <sandrinealcazar@gmail.com>
2025-02-14 14:41:01 +0100

    fix: Correct links to deploy

--- scripts/Factory/RecipeFactory.js
+++ scripts/Factory/RecipeFactory.js
@@ -17,7 +17,8 @@ class Recipe {
 
   get image () {
     return (
-      window.location.origin + '/Lespetitsplats/assets/Photos/Photos_P7_Les_petits_plats/' + `${this._image}`
+     
+     '/assets/Photos/Photos_P7_Les_petits_plats/' + `${this._image}`
     )
   }
 
